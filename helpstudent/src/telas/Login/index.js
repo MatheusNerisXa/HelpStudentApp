478950e1504056200a51ba47317294536c82a457
matheusnerisxavier <matheus.nerisxa@outlook.com>
2023-05-07 15:22:56 -0300

    adicionando mensagem de error na tela de login

--- helpstudent/src/telas/Login/index.js
+++ helpstudent/src/telas/Login/index.js
@@ -1,6 +1,7 @@
 import React, { useState } from 'react';
 import { View } from 'react-native';
 import Botao from '../../componentes/Botao';
+import { Alerta } from '../../componentes/Alerta';
 import { EntradaTexto } from '../../componentes/EntradaTexto';
 import { logar } from '../../service/requisicoesFirebase';
 import estilos from './estilos';
@@ -8,10 +9,26 @@ import estilos from './estilos';
 export default function Login({ navigation }) {
   const [email, setEmail] = useState('');
   const [senha, setSenha] = useState('');
+  const [statusError, setStatusError] = useState('');
+  const [mensagemError, setMensagemError] = useState('');
 
   async function realizarLogin(){
-    const resultado = await logar(email,senha);
-    console.log(resultado)
+    if(email == ''){
+      setMensagemError('O campo e-mail é obrigatório!');
+      setStatusError('email');
+    } else if (senha == ''){
+      setMensagemError('O campo senha é obrigatório!');
+      setStatusError('senha');
+    } else {
+      const resultado = await logar(email,senha);
+      if(resultado == 'erro'){
+        setStatusError('firebase')
+        setMensagemError('Email ou senha inválidos!')
+      }
+      else {
+        navigation.navigate('Principal')
+      }
+    }
   }
 
   return (
@@ -20,14 +37,24 @@ export default function Login({ navigation }) {
         label="E-mail"
         value={email}
         onChangeText={texto => setEmail(texto)}
+        error={statusError == 'email'}
+        messageError={mensagemError}
       />
       <EntradaTexto
         label="Senha"
         value={senha}
         onChangeText={texto => setSenha(texto)}
         secureTextEntry
+        error={statusError == 'senha'}
+        messageError={mensagemError}
       />
       
+      <Alerta
+        mensagem={mensagemError}
+        error={statusError == 'firebase'}
+        setError={setStatusError}
+      />
+
       <Botao onPress={() => realizarLogin()}>LOGAR</Botao>
       <Botao 
         onPress={() => { navigation.navigate('Cadastro') }}

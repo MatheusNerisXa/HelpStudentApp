ff290f185bc820ae64c3884a71312313e4f5e0a0
matheusnerisxavier <matheus.nerisxa@outlook.com>
2023-05-07 13:49:06 -0300

    mensagens de erro no cadastro

--- helpstudent/src/telas/Cadastro/index.js
+++ helpstudent/src/telas/Cadastro/index.js
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from 'react';
-import { View } from 'react-native';
+import { Alert, View } from 'react-native';
 import Botao from '../../componentes/Botao';
 import { EntradaTexto } from '../../componentes/EntradaTexto';
 import estilos from './estilos';
@@ -9,12 +9,35 @@ export default function Cadastro({ navigation }) {
   const [email, setEmail] = useState('');
   const [senha, setSenha] = useState('');
   const [confirmaSenha, setConfirmaSenha] = useState('');
+  const [statusError, setStatusError] = useState('');
+  const [mensagemError, setMensagemError] = useState('');
 
   async function realizaCadastro(){
-    await cadastrar(email,senha,confirmaSenha);
-    setEmail('')
-    setSenha('')
-    setConfirmaSenha('')
+    if(email == ''){
+      setMensagemError('Preencha com o seu email');
+      setStatusError('email');
+    } else if (senha == ''){
+      setMensagemError('Digite sua senha');
+      setStatusError('senha');
+    } else if (confirmaSenha == ''){
+      setMensagemError('Confirme sua senha');
+      setStatusError('confirmaSenha');
+    } else if (confirmaSenha != senha){
+      setMensagemError('As senhas não conferem');
+      setStatusError('confirmaSenha');
+    } else{
+     const resultado =  await cadastrar(email,senha);
+     if(resultado == 'sucesso'){
+      Alert.alert('Usuário cadastro com sucesso!');
+      setEmail('')
+      setSenha('')
+      setConfirmaSenha('')
+     } else {
+      Alert.alert(resultado);
+     }
+     setStatusError('')
+     setMensagemError('')
+    }
   }
 
   return (
@@ -23,12 +46,16 @@ export default function Cadastro({ navigation }) {
         label="E-mail"
         value={email}
         onChangeText={texto => setEmail(texto)}
+        error={statusError == 'email'}
+        messageError={mensagemError}
       />
       <EntradaTexto
         label="Senha"
         value={senha}
         onChangeText={texto => setSenha(texto)}
         secureTextEntry
+        error={statusError == 'senha'}
+        messageError={mensagemError}
       />
 
       <EntradaTexto
@@ -36,6 +63,8 @@ export default function Cadastro({ navigation }) {
         value={confirmaSenha}
         onChangeText={texto => setConfirmaSenha(texto)}
         secureTextEntry
+        error={statusError == 'confirmaSenha'}
+        messageError={mensagemError}
       />
       
       <Botao onPress={() => realizaCadastro()}>CADASTRAR</Botao>
